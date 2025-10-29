180a03d117c52dc70b8c57cabae7bb482ca3a9c8
Dawid <u56128@tudent.wat.edu.pl>
2025-10-29 10:36:49 +0100

    wip: added function to display user info

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+def nazwa_nowej_funkcji(operacja, users_data):
+    match operacja:
+        case "wyswietl":
+            wyswietlanie_znajomych(users_data)
+        case "dodanie":
+            print("Wybano funkcje dodawania znajomego")
+            nowe_imie:str = input("Podaj imie: ")
+            nowa_lokacja:str = input("Podaj lokalizacje: ")
+            nowa_ilosc_postow:int = int(input("Podaj ilość postów: "))
+            users_data.append({
+                "alias_name": nowe_imie,
+                "location": nowa_lokacja,
+                "posts": nowa_ilosc_postow
+            })
+            wyswietlanie_znajomych(users_data)
+
+
+def wyswietlanie_znajomych(users_data_second):
+    for user in users_data_second:
+        print(f"Twój znajomy {user["alias_name"]} z {user["location"]} opublikował {user['posts']} postów")
+
+
 users: list = [{
     "alias_name": "Dawid",
     "location": "Radom",
@@ -16,5 +38,19 @@ users: list = [{
     "posts": 1025
 }]
 
-for user in users:
-    print(f"Twój znajomy {user["alias_name"]} z {user["location"]} opublikował {user['posts']} postów")
+while True:
+    tmp_choice:int = int(input("Wybierz opcje menu: "))
+    match tmp_choice:
+        case 0:
+            break
+        case 1:
+            nazwa_nowej_funkcji("wyswietl")
+        case 2:
+            nazwa_nowej_funkcji("dodanie")
+        case 3:
+            print("Wybrano funkcje usuwania znajomych")
+        case 4:
+            print("Wybrano funkcje aktualizowania znajomych")
+
+
+
