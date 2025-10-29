5b63c517b3450ca97665d687e9829fc30d5dd33a
Dawid <u56128@tudent.wat.edu.pl>
2025-10-29 09:41:07 +0100

    wip: list of names, locations and posts

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-zmienna_1:str = "Dawid"
-zmienna_2:str = "Kasia"
-zmienna_3:str = "Michasia"
-zmienna_4:str = "Asia"
+users: list = [{
+    "alias_name": "Dawid",
+    "location": "Radom",
+    "posts": 149
+}, {
+    "alias_name": "Kasia",
+    "location": "Warszawa",
+    "posts": 37
+}, {
+    "alias_name": "Michasia",
+    "location": "Kraków",
+    "posts": 1
+}, {
+    "alias_name": "Asia",
+    "location": "Gdańsk",
+    "posts": 1025
+}]
 
-#Przerob istniejacy kod w taki sposob zeby
-uzytkownicy:list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4]
-for uzytkownik in uzytkownicy:
-    print(f"Hello {uzytkownik}")
+for user in users:
+    print(f"Twój znajomy {user["alias_name"]} z {user["location"]} opublikował {user['posts']} postów")
