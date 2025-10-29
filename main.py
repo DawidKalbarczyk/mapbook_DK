3828b190441dccfe01e0a486b3b9159f7c36f7ae
Dawid <u56128@tudent.wat.edu.pl>
2025-10-29 09:10:20 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+zmienna_1:str = "Dawid"
+zmienna_2:str = "Kasia"
+zmienna_3:str = "Michasia"
+zmienna_4:str = "Asia"
+
+#Przerob istniejacy kod w taki sposob zeby
+uzytkownicy:list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+for uzytkownik in uzytkownicy:
+    print(f"Hello {uzytkownik}")
